7bafb71ca1b68b108c54add3bfb37ad39c0c1b57
Bayanda Mnguni <bayandaPM@gmail.com>
2026-03-18 14:16:21 +0200

    updated: auth provider wired

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
 import "./index.css";
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
